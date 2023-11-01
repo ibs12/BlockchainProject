a0df556f920c893c66ede5357877ff81d6ef4e79
ibs12 <89870752+ibs12@users.noreply.github.com>
2023-11-01 13:36:46 -0400

    fixed compiler issue

--- ERC20SmartContract.sol
+++ ERC20SmartContract.sol
@@ -5,11 +5,10 @@ import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
 
 contract GoldCoin is ERC20, Ownable {
-    constructor(
-        address initialOwner
-    ) ERC20("GoldCoin", "GC") Ownable(initialOwner) {
+    constructor(address initialOwner) ERC20("GoldCoin", "GC") Ownable(initialOwner) {
         _mint(initialOwner, 1000000 * 10 ** decimals());
     }
+
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
