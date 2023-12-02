e0fd4ecf007efe155d9aec88d4726c9a203d3961
ibs12 <89870752+ibs12@users.noreply.github.com>
2023-12-02 13:20:54 -0500

    Added batchBalance,walletBalance,safeBatchTransfer

--- DApp-contract/ERC1155SmartContract.sol
+++ DApp-contract/ERC1155SmartContract.sol
@@ -4,14 +4,13 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts@4.0.0/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts@4.0.0/utils/Counters.sol";
 import "@openzeppelin/contracts@4.0.0/access/Ownable.sol";
-import "./ERC20SmartContract.sol"; // Importing the local ERC20 contract.
-import "./ERC721SmartContract.sol"; // Importing the local ERC721 contract.
+import "./ERC20SmartContract.sol";
+import "./ERC721SmartContract.sol";
 
 contract GameItems is ERC1155, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
 
-    // Addresses of the deployed ERC20 and ERC721 contracts
     GoldCoin private erc20Token;
     UniqueCards private erc721Token;
 
@@ -114,10 +113,10 @@ contract GameItems is ERC1155, Ownable {
         uint256 price;
 
         if (items[itemId].itemType == ItemType.PowerUp) {
-            price = 1 * 1e18; // Adjust the price as necessary
-            playerPowerUps[msg.sender] += 1; // Increment the player's power-up count
+            price = 1 * 1e18;
+            playerPowerUps[msg.sender] += 1;
         } else if (items[itemId].itemType == ItemType.Mod) {
-            price = 2 * 1e18; // Adjust the price as necessary
+            price = 2 * 1e18;
         }
 
         require(msg.value >= price, "Insufficient ETH sent.");
@@ -127,11 +126,11 @@ contract GameItems is ERC1155, Ownable {
     }
 
     function purchaseWithGoldCoin(uint256 itemId) external {
-        uint256 price = 30 * 10 ** 18; // Adjust the price as necessary
+        uint256 price = 30 * 10 ** 18;
         erc20Token.transferFrom(msg.sender, address(this), price);
 
         if (items[itemId].itemType == ItemType.PowerUp) {
-            playerPowerUps[msg.sender] += 1; // Increment the player's power-up count
+            playerPowerUps[msg.sender] += 1;
         }
 
         _mint(msg.sender, itemId, 1, "");
@@ -145,7 +144,7 @@ contract GameItems is ERC1155, Ownable {
         uint256[] memory cardIds = new uint256[](cardCount);
         for (uint256 i = 0; i < cardCount; i++) {
             uint256 cardId = erc721Token.tokenOfOwnerByIndex(player, i);
-            erc721Token.increaseCardDefense(cardId, 10); // Modify the attribute increase as needed
+            erc721Token.increaseCardDefense(cardId, 10);
             cardIds[i] = cardId;
         }
         emit ModPowerUpUsed(player, cardIds);
@@ -170,6 +169,71 @@ contract GameItems is ERC1155, Ownable {
 
     function hasPowerUp(address player) external view returns (bool) {
         return playerPowerUps[player] > 0;
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override {
+        require(
+            from == _msgSender() || isApprovedForAll(from, _msgSender()),
+            "ERC1155: caller is not owner nor approved"
+        );
+        safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    function getBatchBalance(
+        address account,
+        uint256[] memory ids
+    ) public view returns (uint256[] memory) {
+        address[] memory accountArray = new address[](ids.length);
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            accountArray[i] = account;
+        }
+
+        return balanceOfBatch(accountArray, ids);
+    }
+
+    function getWalletBalance(
+        address user
+    )
+        public
+        view
+        returns (uint256[] memory, uint256, uint256[] memory, uint256, uint256)
+    {
+        // ERC1155 Balances
+        uint256[] memory erc1155Balances = new uint256[](_itemIds.current());
+        for (uint256 i = 1; i <= _itemIds.current(); i++) {
+            // Start from 1 since item IDs start from 1
+            erc1155Balances[i - 1] = balanceOf(user, i);
+        }
+
+        // ERC20 Balance
+        uint256 erc20Balance = erc20Token.balanceOf(user);
+
+        // ERC721 Balances
+        uint256[] memory erc721Balances = new uint256[](
+            erc721Token.balanceOf(user)
+        );
+        for (uint256 i = 0; i < erc721Token.balanceOf(user); i++) {
+            erc721Balances[i] = erc721Token.tokenOfOwnerByIndex(user, i);
+        }
+
+        // PowerUps and ModPowerUps
+        uint256 powerUpCount = playerPowerUps[user];
+        uint256 modPowerUpCount = playerModPowerUps[user];
+
+        return (
+            erc1155Balances,
+            erc20Balance,
+            erc721Balances,
+            powerUpCount,
+            modPowerUpCount
+        );
     }
 
     receive() external payable {}
